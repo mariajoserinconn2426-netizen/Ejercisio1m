b2ba8baba9578ebcfa50caddebe196d8978a98b2
mariajoserinconn2426-netizen <mariajoserinconn2426@gmail.com>
2026-05-14 01:01:18 +0000

    bucle for

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ while continuar == "si":
     nombre = input("Ingrese el nombre del estudiante: ")
 
     suma = 0
+        # Bucle for para pedir las 5 notas
+    for i in range(5):
+        nota = float(input(f"Ingrese la nota {i+1}: "))
+        suma = suma + nota
+
