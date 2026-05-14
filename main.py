2567b7891a0beb49a5d3074766233c2bbb0d0dc7
mariajoserinconn2426-netizen <mariajoserinconn2426@gmail.com>
2026-05-14 00:59:28 +0000

    Variables y Datos

---
+++ main.py
@@ -0,0 +1,9 @@
+# Programa para evaluar el rendimiento académico de estudiantes
+
+continuar = "si"
+
+while continuar == "si":
+
+    nombre = input("Ingrese el nombre del estudiante: ")
+
+    suma = 0
